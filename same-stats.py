b96f955426ae1f6f052df658b18ad7d838363cee
Albert Tam <tamalbert8@gmail.com>
2020-05-28 11:22:42 -0700

    readability fixes, added more command-line args

--- same-stats.py
+++ same-stats.py
@@ -9,7 +9,15 @@ import argparse
 
 # Default number of iterations to run the genetic algorithm, if no argument is specified
 DEFAULT_ITERATIONS = 150
+# Default population size for the genetic algorithm, if no argument is specified
 DEFAULT_POPSIZE = 350
+# Default probability of crossover for the genetic algorithm, if no argument is specified
+DEFAULT_PCROSSOVER = 0.8
+# Default probability of mutation for the genetic algorithm, if no argument is specified
+DEFAULT_PMUTATION = 0.01
+# Default standard deviation for mutations for the genetic algorithm; increasing will increase the likelihood of large mutations
+DEFAULT_MUTATIONSD = 0.25
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', help='specify the filename to read data from')
@@ -17,7 +25,12 @@ parser.add_argument('-i', '--iterations', type=int, default=DEFAULT_ITERATIONS,
     help='the number of iterations to run each genetic algorithm')
 parser.add_argument('-p', '--population', type=int, default=DEFAULT_POPSIZE,
     help='size of population of genes for genetic algorithm')
-
+parser.add_argument('-c', '--pcrossover', type=int, default=DEFAULT_PCROSSOVER,
+    help='probability of crossover for genetic algorithm')
+parser.add_argument('-m', '--pmutation', type=int, default=DEFAULT_PMUTATION,
+    help='probability of mutation for genetic algorithm')
+parser.add_argument('-s', '--mutationsd', type=int, default=DEFAULT_MUTATIONSD,
+    help='standard deviation of normal distribution for mutations for genetic algorithm')
 
 args = parser.parse_args()
 
@@ -58,9 +71,6 @@ def rand_normal_data(n):
        row[:] = fix_stats(row, 0, 1)
     return A
 
-# L is the matrix ((1, 0), (r, sqrt(1 - r^2))). When multiplied by an uncorrelated 2 x n matrix, the correlation of the two
-# rows becomes r.
-
 # Fit the summary statstics of "data" to match those of "ref"
 def fit_summary_statistics_of_to(data, ref):
     xbar, ybar, sx, sy, r, n = summary(ref)
@@ -76,14 +86,14 @@ def fit_summary_statistics_of_to(data, ref):
         fix_stats(A[1], 0, 1)
     ])
 
-    # Obtain the correct correlation matrix
+    # Obtain the correct correlation matrix to multiply by
     L = np.array([
         [1, 0],
         [r, sqrt(1 - r ** 2)]
     ])
     
     # Multiply correlation matrix by A to force correct value of r
-    A = np.matmul(L, A)
+    A = L @ A
     
     # Set mean and variances of data to reference distribution
     A = np.array([
@@ -96,29 +106,30 @@ def fit_summary_statistics_of_to(data, ref):
 # Following method generates a set of REF_N datapoints with almost identical summary statistics to the reference
 def replicate_statistics(ref):
     xbar, ybar, sx, sy, r, n = summary(ref)
-    # Generate random normal data
+    # Generate random normal data, then fit summary statistics
     return fit_summary_statistics_of_to(rand_normal_data(n), ref)
 
 def crossover(data1, data2, p_crossover):
     if np.random.rand() < p_crossover:
         crossover_point = np.random.randint(data1.shape[1] + 1) # pick random column for crossover
-        left1 = data1[:, :crossover_point]
-        right1 = data2[:, crossover_point:]
+        first_left_slice = data1[:, :crossover_point]
+        second_right_slice = data2[:, crossover_point:]
+        first_offspring = np.concatenate((first_left_slice, second_right_slice), axis=1)
 
-        right2 = data1[:, crossover_point:]
-        left2 = data2[:, :crossover_point]
-        return (np.concatenate((left1, right1), axis=1), np.concatenate((left2, right2), axis=1))
+        first_right_slice = data1[:, crossover_point:]
+        second_left_slice = data2[:, :crossover_point]
+        second_offspring = np.concatenate((first_right_slice, second_left_slice), axis=1)
+        return (first_offspring, second_offspring)
     else:
         return (data1, data2)
 
-def mutate(data, p_mutation):
+def mutate(data, p_mutation, mutation_sd):
     if np.random.rand() < p_mutation:
-        MUTATE_SD = 0.25
-        return data + np.random.normal(0, MUTATE_SD, data.shape)
+        return data + np.random.normal(0, mutation_sd, data.shape)
     else:
         return data
 
-def genetic_step(population, fitness, p_crossover, p_mutation):
+def genetic_step(population, fitness, p_crossover, p_mutation, mutation_sd):
     # Calculate fitnesses
     fitnesses = [fitness(gene) for gene in population]
     print(max(fitnesses))
@@ -128,7 +139,7 @@ def genetic_step(population, fitness, p_crossover, p_mutation):
     # Create new population first
     new_population = []
 
-    # Elitism
+    # Elitism: best-performing gene gets passed down to new population automatically
     max_fitness_idx = np.argmax(fitnesses)
     new_population.append(population[max_fitness_idx])
 
@@ -141,18 +152,19 @@ def genetic_step(population, fitness, p_crossover, p_mutation):
         # make random choice of parents from selected_genes
         parent_indices = np.random.choice(range(len(selected_genes)), size=2)
         parents = [selected_genes[idx] for idx in parent_indices]
-        offspring_1, offspring_2 = crossover(*parents, p_crossover)
-        new_population.append(offspring_1)
-        new_population.append(offspring_2)
+        offspring = crossover(*parents, p_crossover)
+        new_population.extend(offspring)
 
     # Mutation
-    new_population = [mutate(gene, p_mutation) for gene in new_population]
+    new_population = [mutate(gene, p_mutation, mutation_sd) for gene in new_population]
 
-    # We may need to trim randomly from new_population to keep population sizes stable.
+    # We may need to trim randomly from new_population to keep population sizes stable, since each crossover step
+    # produces 2 offspring, but elitism places 1 offspring in the population. Without this step, population size would
+    # increase by 1 after each genetic step
     if len(new_population) > len(population):
-        # We prefer to cut off offspring
+        # We prefer to cut off offspring, not the elite gene (which is guaranteed to be at index 0)
         offset = len(population) - len(new_population)
-        new_population = new_population[:offset]
+        new_population = new_population[:offset] # Cuts off number of elements equal to offset
 
     return new_population
 
@@ -171,7 +183,7 @@ for diff in measures_to_test:
     print('––––STARTING GENETIC FOR DISSIMILARITY MEASURE:', diff)
     for i in range(args.iterations):
         print(i)
-        population = genetic_step(population, partial(diff, REFERENCE), 0.8, 0.01)
+        population = genetic_step(population, partial(diff, REFERENCE), args.pcrossover, args.pmutation, args.mutationsd)
         # Stat fix
         population = [fit_summary_statistics_of_to(gene, REFERENCE) for gene in population]
     all_populations.append(population)
