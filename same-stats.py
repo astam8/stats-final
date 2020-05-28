f8da6a3498acaf3bbcaf4c80d860a08b6f906ef0
Albert Tam <tamalbert8@gmail.com>
2020-05-27 22:34:28 -0700

    separated data from code, cleaned up functions

--- same-stats.py
+++ same-stats.py
@@ -6,30 +6,16 @@ from math import sqrt
 from functools import partial
 import dissimilarity as ds
 
-REFERENCE = np.array([[-0.83375003,  1.0954705 , -0.05082271, -0.22376648,  0.36000685,
-        -0.09391634,  0.2156405 , -0.93300305, -1.54057091, -0.48787898,
-        -1.67570942,  0.17950772,  0.35156441,  1.60994239,  0.025276  ,
-         2.57038455,  0.55633747, -0.86154288, -0.88514176,  0.62197216],
-       [-1.28014033,  0.50484594,  0.42892175, -1.14352745,  0.78320453,
-         0.40341267,  0.88552499, -1.26098764, -0.48010823, -1.08377086,
-        -0.49397959,  0.21351545, -0.29116993,  1.72080078, -0.0695087 ,
-         1.05075295,  1.08878165, -1.1260957 , -0.13119122,  0.28071896]])
+def load_data(filename):
+    with open(filename, 'r') as fin:
+        lines = fin.read().split('\n')
+        x, y = zip(*[line.split(' ') for line in lines])
+        x = [float(val) for val in x]
+        y = [float(val) for val in y]
+        
+    return np.array([x, y])
 
-'''REFERENCE = np.array([[-0.26726124, -0.26726124, -0.26726124, -0.26726124, -0.26726124,
-        -0.26726124, -0.26726124, -0.26726124, -0.26726124, -0.26726124,
-        -0.26726124, -0.26726124, -0.26726124, -0.26726124,  3.74165739],
-       [-1.33808113, -1.11256184, -0.88704255, -0.66152326, -0.43600397,
-        -0.21048467,  0.01503462,  0.24055391,  0.4660732 ,  0.6915925 ,
-         0.91711179,  1.14263108,  1.36815037,  1.59366967, -1.78911972]])'''
-
-'''REFERENCE = np.array([[ 1.43375611,  2.04441281,  1.41975531, -0.0815076 , -0.36698188,
-        -0.36245928,  0.11546782, -0.7661847 , -0.88579085, -1.94706537,
-         0.575897  , -0.29487172,  0.00660205, -0.93777525,  0.04674554],
-       [-0.031157  ,  0.75224918,  1.28054373, -1.43668944,  1.12769705,
-        -0.41057733,  0.32079744, -1.31533635, -1.76772165, -0.72650318,
-         0.53151029,  0.90611511,  0.45094862,  1.26491801, -0.94679448]])'''
-# Number of datasets to randomly generate
-NUM_DATASETS = 10
+REFERENCE = load_data('dataset1.txt')
 
 # Get summary statistics of a list of data
 def summary(data):
@@ -50,85 +36,53 @@ def fix_stats(data, mean, std):
     data = (data * std) + mean
     return data
 
-# Generate random data from a normal distribution
+# Generate random 2-variable data with n data points with mean 0 and standard deviation 1, and return as an (2 x n) np.array
 def rand_normal_data(n):
     A = np.random.rand(2, n)
-    A[0] = fix_stats(A[0], 0, 1)
-    A[1] = fix_stats(A[1], 0, 1)
+    for row in A:
+       row[:] = fix_stats(row, 0, 1)
     return A
 
 # L is the matrix ((1, 0), (r, sqrt(1 - r^2))). When multiplied by an uncorrelated 2 x n matrix, the correlation of the two
 # rows becomes r.
-L = np.array([
-    [1, 0],
-    [REF_R, sqrt(1 - REF_R ** 2)]
-])
+
+# Fit the summary statstics of "data" to match those of "ref"
+def fit_summary_statistics_of_to(data, ref):
+    xbar, ybar, sx, sy, r, n = summary(ref)
+
+    A = data
+    
+    # Orthogonalize A
+    A[1] = A[1] - (np.dot(A[0], A[1]) / (np.linalg.norm(A[0]) ** 2))*A[0]
+    
+    # Normalize A
+    A = np.array([
+        fix_stats(A[0], 0, 1),
+        fix_stats(A[1], 0, 1)
+    ])
+
+    # Obtain the correct correlation matrix
+    L = np.array([
+        [1, 0],
+        [r, sqrt(1 - r ** 2)]
+    ])
+    
+    # Multiply correlation matrix by A to force correct value of r
+    A = np.matmul(L, A)
+    
+    # Set mean and variances of data to reference distribution
+    A = np.array([
+        fix_stats(A[0], xbar, sx),
+        fix_stats(A[1], ybar, sy)
+    ])
+    
+    return A
 
 # Following method generates a set of REF_N datapoints with almost identical summary statistics to the reference
 def replicate_statistics(ref):
     xbar, ybar, sx, sy, r, n = summary(ref)
     # Generate random normal data
-    A = rand_normal_data(n)
-    
-    # Orthogonalize A
-    A[1] = A[1] - (np.dot(A[0], A[1]) / (np.linalg.norm(A[0]) ** 2))*A[0]
-    
-    # Normalize A
-    A = np.array([
-        fix_stats(A[0], 0, 1),
-        fix_stats(A[1], 0, 1)
-    ])
-
-    # Obtain the correct correlation matrix
-    L = np.array([
-        [1, 0],
-        [r, sqrt(1 - r ** 2)]
-    ])
-    
-    # Multiply correlation matrix by A to force correct value of r
-    A = np.matmul(L, A)
-    
-    # Set mean and variances of data to reference distribution
-    A = np.array([
-        fix_stats(A[0], xbar, sx),
-        fix_stats(A[1], ybar, sy)
-    ])
-    
-    return A
-
-# Genetic approach
-
-def fit_summary_statistics_of_to(data, ref):
-    xbar, ybar, sx, sy, r, n = summary(ref)
-    # Generate random normal data
-    A = fix_stats(data, 0, 1)
-    
-    # Orthogonalize A
-    A[1] = A[1] - (np.dot(A[0], A[1]) / (np.linalg.norm(A[0]) ** 2))*A[0]
-    
-    # Normalize A
-    A = np.array([
-        fix_stats(A[0], 0, 1),
-        fix_stats(A[1], 0, 1)
-    ])
-
-    # Obtain the correct correlation matrix
-    L = np.array([
-        [1, 0],
-        [r, sqrt(1 - r ** 2)]
-    ])
-    
-    # Multiply correlation matrix by A to force correct value of r
-    A = np.matmul(L, A)
-    
-    # Set mean and variances of data to reference distribution
-    A = np.array([
-        fix_stats(A[0], xbar, sx),
-        fix_stats(A[1], ybar, sy)
-    ])
-    
-    return A
-
+    return fit_summary_statistics_of_to(rand_normal_data(n), ref)
 
 def crossover(data1, data2, p_crossover):
     if np.random.rand() < p_crossover:
@@ -187,8 +141,7 @@ def genetic_step(population, fitness, p_crossover, p_mutation):
 
     return new_population
 
-
-ITERATIONS = 150
+ITERATIONS = 1
 POP_SIZE = 350
 
 # Plotting
@@ -199,7 +152,9 @@ all_populations = [REFERENCE]
 population = [replicate_statistics(REFERENCE) for i in range(POP_SIZE)]
 population = [fit_summary_statistics_of_to(gene, REFERENCE) for gene in population]
 
-for diff in (ds.data_diff, ds.skewness_diff, ds.kurt_diff, ds.power_diff):
+measures_to_test = (ds.data_diff, ds.skewness_diff, ds.kurt_diff, ds.power_diff)
+
+for diff in measures_to_test:
     population = [replicate_statistics(REFERENCE) for i in range(POP_SIZE)]
     print('––––STARTING GENETIC FOR DISSIMILARITY MEASURE:', diff)
     for i in range(ITERATIONS):
@@ -212,13 +167,9 @@ for diff in (ds.data_diff, ds.skewness_diff, ds.kurt_diff, ds.power_diff):
     print(population[0])
     print('reference:', summary(REFERENCE))
 
-for i in range(5):
+for i in range(len(measures_to_test) + 1):
     ax = plt.subplot(2, 3, i + 1)
     ax.scatter(all_populations[i][0], all_populations[i][1], s=5)
-#plt.scatter(REFERENCE[0], REFERENCE[1])
-#plt.scatter(population[0][0], population[0][1])
-
-
 
 plt.ioff()
 plt.show()
